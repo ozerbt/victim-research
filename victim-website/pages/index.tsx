6a86482e8be09de5c301118b3ec6dda8a18f31e8
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-02-23 15:41:16 -0500

    refactoring project

--- victim-website/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import HeroSection from '../components/Sections/HeroSection'
-export default function index() {
-  return (
-    <div>
-      <HeroSection />
-    </div>
-  )
-}
